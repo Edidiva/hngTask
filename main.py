94fbfbb52827c8b3fa03318a87cdcfa8e0556ada
Edikan <edikanakpan5@gmail.com>
2022-10-28 18:44:12 +0100

    update

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return { "slackUsername": 'Edikan', "backend":True, "age": 26, "bio": "I am tech ethusiast finding my way in this digital world of possibilities"}
+    return { "slackUsername": 'Edikan', "backend":True, "age": 26, "bio": "I am a tech ethusiast finding my way in this digital world of possibilities"}
     
